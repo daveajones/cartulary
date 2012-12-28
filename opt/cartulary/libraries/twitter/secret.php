22580048cc9ff421bc7e92c0d072bd077bde0961
Dave Jones <sect62@gmail.com>
2012-12-28 17:20:32 +0000

    added apache site config template

--- opt/cartulary/libraries/twitter/secret.php
+++
@@ -1,4 +0,0 @@
-<?php
-$consumer_key = '<PLACE YOUR CONSUMER KEY HERE>';
-$consumer_secret = '<PLACE YOUR CONSUMER SECRET KEY HERE>';
-?>
--- opt/cartulary/scripts/search-Microblog-temp.js
+++
@@ -1,6 +0,0 @@
-<ul>
- {{each(r, result) data}}
- <li><a href="${result.url}">${result.title}</a></li>
- {{/each}}
-</ul>
-

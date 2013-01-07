f62cb5e0176974ec8e0df0ac7d5c28c016842027
Dave Jones <sect62@gmail.com>
2013-01-07 19:46:38 +0000

    carry old conf values over during upgrade

--- bin/confcheck.php
+++ bin/confcheck.php
@@ -14,8 +14,28 @@
     $cfname = "$confroot/conf/cartulary.conf";
     $cftemp = "$confroot/$templates/cartulary.conf";
 
+    //Default values
+    $l_dbusername = "cartulary";
+    $l_dbpassword = "cartulary";
+    $l_fqdn = "localhost";
+    $l_s3key = "";
+    $l_s3secret = "";
+    $l_s3bucket = "";
+    $l_s3cname = "";
+    $l_s3backup = "";
+
     //If there is already a config file, let's hang on to it
     if( file_exists($cfname) ) {
+      //Pull in the existing values
+      $l_dbusername = $dbuser;
+      $l_dbpassword = $dbpass;
+      $l_fqdn = $system_fqdn;
+      $l_s3key = $s3_sys_key;
+      $l_s3secret = $s3_sys_secret;
+      $l_s3bucket = $s3_sys_bucket;
+      $l_s3cname = $s3_sys_cname;
+      $l_s3backup = $s3_sys_backup;
+
       rename( $cfname, $cfname.'.old.'.time() );
     }
 
@@ -24,38 +44,38 @@
     $template = fread($fh, filesize($cftemp));
 
     //Replace the tags
-    echo "What is your mysql username? [cartulary]: ";
+    echo "What is your mysql username? [$l_dbusername]: ";
     $response = get_user_response();
     if( empty($response) ) {  $response = 'cartulary';  }
     $template = str_replace('dbusernamegoeshere', $response, $template);
 
-    echo "What is your mysql password? [cartulary]: ";
+    echo "What is your mysql password? [$l_dbpassword]: ";
     $response = get_user_response();
     if( empty($response) ) {  $response = 'cartulary';  }
     $template = str_replace('dbpasswordgoeshere', $response, $template);
 
-    echo "What is the fully qualified hostname of your server? [required]: ";
+    echo "What is the fully qualified hostname of your server? [$l_fqdn]: ";
     $response = get_user_response();
     $template = str_replace('domain.goes.here', $response, $template);
     $template = str_replace('fqdn.goes.here', $response, $template);
 
-    echo "What is your Amazon S3 key? [enter to skip]: ";
+    echo "What is your Amazon S3 key? [$l_s3key]: ";
     $response = get_user_response();
     $template = str_replace('s3keyvalue', $response, $template);
 
-    echo "What is your Amazon S3 secret? [enter to skip]: ";
+    echo "What is your Amazon S3 secret? [$l_s3secret]: ";
     $response = get_user_response();
     $template = str_replace('s3secretvalue', $response, $template);
 
-    echo "What S3 bucket do you want user info stored in? [enter to skip]: ";
+    echo "What S3 bucket do you want user info stored in? [$l_s3bucket]: ";
     $response = get_user_response();
     $template = str_replace('s3userbucketvalue', $response, $template);
 
-    echo "Do you have a dns CNAME pointed to this bucket? If so, what is it?  [enter to skip]: ";
+    echo "Do you have a dns CNAME pointed to this bucket? If so, what is it?  [$l_s3cname]: ";
     $response = get_user_response();
     $template = str_replace('s3cnamevalue', $response, $template);
 
-    echo "What S3 bucket do you want backups stored in? [enter to skip]: ";
+    echo "What S3 bucket do you want backups stored in? [$l_s3backup]: ";
     $response = get_user_response();
     $template = str_replace('s3backupbucketvalue', $response, $template);
 

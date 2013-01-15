55520d851da965da5ca4b2cb8292539406970440
Dave Jones <sect62@gmail.com>
2013-01-15 15:46:02 +0000

    added initial skeleton scripts for yo udp server and client

---
+++ bin/yocli.php
@@ -0,0 +1,52 @@
+<?
+
+/*
+        Simple php udp socket client
+*/
+
+//Reduce errors
+error_reporting(~E_WARNING);
+
+$server = gethostbyname('cart.southernbread.org');
+$port = 8904;
+
+if(!($sock = socket_create(AF_INET, SOCK_DGRAM, 0)))
+{
+        $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+
+    die("Couldn't create socket: [$errorcode] $errormsg \n");
+}
+
+echo "Socket created \n";
+
+//Communication loop
+while(1)
+{
+        //Take some input to send
+        echo 'Enter a message to send : ';
+        $input = fgets(STDIN);
+
+        //Send the message to the server
+        if( ! socket_sendto($sock, $input , strlen($input) , 0 , $server , $port))
+        {
+                $errorcode = socket_last_error();
+                $errormsg = socket_strerror($errorcode);
+
+                die("Could not send data: [$errorcode] $errormsg \n");
+        }
+
+        //Now receive reply from server and print it
+        if(socket_recv ( $sock , $reply , 2045 , MSG_WAITALL ) === FALSE)
+        {
+                $errorcode = socket_last_error();
+                $errormsg = socket_strerror($errorcode);
+
+                die("Could not receive data: [$errorcode] $errormsg \n");
+        }
+
+        echo "Reply : $reply";
+}
+
+?>
+
---
+++ bin/yosrv.php
@@ -0,0 +1,43 @@
+<?
+
+//Reduce errors
+error_reporting(~E_WARNING);
+
+//Create a UDP socket
+if(!($sock = socket_create(AF_INET, SOCK_DGRAM, 0)))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+
+    die("Couldn't create socket: [$errorcode] $errormsg \n");
+}
+echo "Socket created \n";
+
+
+// Bind the source address
+if( !socket_bind($sock, "0.0.0.0" , 8904) )
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+
+    die("Could not bind socket : [$errorcode] $errormsg \n");
+}
+echo "Socket bind OK \n";
+
+
+//Do some communication, this loop can handle multiple clients
+while(1)
+{
+	echo "Waiting for data ... \n";
+	//Receive some data
+	$r = socket_recvfrom($sock, $buf, 512, 0, $remote_ip, $remote_port);
+	echo "$remote_ip : $remote_port -- " . $buf;
+
+	//Send back the data to the client
+	socket_sendto($sock, "OK " . $buf , 100 , 0 , $remote_ip , $remote_port);
+}
+
+
+socket_close($sock);
+
+?>

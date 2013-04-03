cb6025bdaa0d27de7d3d1744fb1b4f0d9f282d57
Dave Jones <sect62@gmail.com>
2013-04-03 21:52:44 +0000

    genericize the uploadifive stuff

---
+++ scripts/upload.js
@@ -0,0 +1,90 @@
+<?if($device!="android") {?>
+var enclist = new Array();
+$(function() {
+    $('#file_upload').uploadifive({
+        'auto'         : true,
+        'method'       : 'post',
+        'dnd'          : true,
+        'queueID'      : 'queue',
+        'uploadScript' : '/cgi/in/upload',
+        'simUploadLimit'   : 1,
+        'formData'     : {
+            'datestamp'  : gDatestamp
+	},
+	'onSelect'     : function(queue) {
+            $('#divEnclosures').show();
+	},
+        'onAddQueueItem'   : function(file) {
+            console.log(file);
+	    if( $.inArray(file.name, enclist) > -1 ) {
+		$('#file_upload').uploadifive('cancel', file, true);		
+	    }
+        },
+	'onClearQueue' : function(queue) {
+	    enclist = [];
+	    $('input.postenclosure').remove();
+	},
+	'onCancel'     : function(file) {
+	    $("input.postenclosure[data-filename='" + file.name + "'").remove();
+            enclist.splice($.inArray(file.name, enclist), 1);
+        },
+        'onUploadComplete' : function(file, data) {
+	    var jdata = $.parseJSON(data);
+            $('.complete .filename:contains("' + file.name + '")').each(function(index) {
+	    	if( isImage(jdata.url) ) {
+	              $(this).parent().append('<a href="' + jdata.url + '" title="Click to embiggen."><img class="imgenclosure" src="' + jdata.url + '" alt="" /></a>');
+		      $(this).parent().children('span.fileinfo').hide();
+                }
+                $(this).html('Link: <a href="' + jdata.url + '" title="Click to embiggen.">' + jdata.url + '</a>');
+                $(this).parent().append('<input type="hidden" class="postenclosure" data-filename="' + file.name + '" name="enclosure[]" />');
+                $(this).parent().children('input').val(data);
+		
+		enclist.push(file.name);
+	    });
+        }
+    })
+});
+<?}?>
+
+//Unhide the enclosure section
+function showEnclosures() {
+	$('#divEnclosures').show();
+}
+
+$(document).ready( function() {
+		//Set the datestamp
+		$('#divEnclosures .datestamp').val(gDatestamp);
+
+		//Reveal the enclosure section upon change
+		$('#fileMobile').change(function() {
+			$('#divEnclosures').show();
+			$('#divEnclosures #spnQueueText').text('File selected: ' + $('#fileMobile').val());
+		});
+
+		//The enclosure attachment button click handler
+		$('#btnAttachFile').bind('click', function() {
+			$('#divUpload').show();
+		});
+
+		//Start an enclosure upload
+                <?if( $device != "android" ) {?>
+		$('#aUploadTrigger').bind('click', function() {
+			$('#divEnclosures').show();
+			$('#file_upload').uploadifive('upload');
+		});
+                <?}?>
+
+		//Attach removers to extra elements
+		$('.aRemoveListItem').click(function() {
+			$(this).parent('li').remove();
+		});
+
+		//Expose drop zone when a drag and drop comes in
+		<?if( $section != "Microblog" ) {?>
+		$('body').on('dragenter', function() {
+			if( $('#mdlMicroblogPost').is(':visible') ) {
+				showEnclosures();
+			}
+		});
+		<?}?>
+});

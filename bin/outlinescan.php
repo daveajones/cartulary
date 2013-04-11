eb6360df49c9e85f604ca8093bd0c97b71a10387
Dave Jones <sect62@gmail.com>
2013-04-11 15:53:50 +0000

    only ping each server once per outline scan

--- bin/outlinescan.php
+++ bin/outlinescan.php
@@ -7,6 +7,7 @@
 
   //Globals and flags
   $ouchange = FALSE;
+  $servcheck = array();
 
   //Get the outline list
   $outlines = get_all_outlines();
@@ -17,115 +18,168 @@
     loggit(1, "Checking outline: [".$outline['title']." | ".$outline['url']."]...");
     echo "Checking outline: [".$outline['title']." | ".$outline['url']."]...\n";
 
+    //How many people subscribe to this outline?
     $subcount = get_outline_subscriber_count($outline['id']);
     loggit(1, "Outline: [".$outline['title']."] has: [$subcount] subscribers.");
     echo "    Outline: [".$outline['title']."] has: [$subcount] subscribers.\n";
-    //echo print_r(get_outline_subscribers($outline['id']), true)."\n";
+
+    //Skip outlines with zero subscribers
+    if( $subcount == 0 ) {  continue;  }
 
     //Set the purge bit on all of this outline's feeds to true
     $purgecount = mark_all_outline_feeds_to_purge($outline['id']);
     echo "    Marked $purgecount feeds from outline: [".$outline['title']."] to purge.\n\n";
 
-    //We don't want to scan subscriberless outlines or local lists
-    if($subcount > 0 && $outline['type'] != 'list') {
-    	$result = get_outline_items($outline['id']);
-	echo "    Got [".count($result)."] items back from the get_outline_items() call.\n";
+    //Get the new content from the outline
+    $result = get_outline_items($outline['id']);
+    echo "    Got [".count($result)."] items back from the get_outline_items() call.\n";
+    if($result == -1) {
+    	loggit(2, "Error getting items for outline: [".$outline['title']." | ".$outline['url']."]");
+        echo "    Error getting items for outline: [".$outline['title']." | ".$outline['url']."]\n";
+        unmark_all_outline_feeds_to_purge($outline['id']);
+	continue;
+    } else if ($result == -2) {
+	loggit(1, "Outline: [".$outline['title']." | ".$outline['url']."] has no items.");
+	echo "    Outline is empty.\n";
+	unmark_all_outline_feeds_to_purge($outline['id']);
+	continue;
+    } else if ($result == -3) {
+      	loggit(1, "Outline: [".$outline['title']." | ".$outline['url']."] is current.");
+      	echo "    Outline is current.\n";
+        unmark_all_outline_feeds_to_purge($outline['id']);
+	continue;
+    } else {
+      	loggit(1, "Outline: [".$outline['title']." | ".$outline['url']."] updated.");
+      	echo "    Outline updated.\n";
+    }
 
-    	if($result == -1) {
-	       	loggit(2, "Error getting items for outline: [".$outline['title']." | ".$outline['url']."]");
-	      	echo "    Error getting items for outline: [".$outline['title']." | ".$outline['url']."]\n";
-	        unmark_all_outline_feeds_to_purge($outline['id']);
-    	} else if ($result == -2) {
-	      	loggit(1, "Outline: [".$outline['title']." | ".$outline['url']."] has no items.");
-	      	echo "    Outline is empty.\n";
-	        unmark_all_outline_feeds_to_purge($outline['id']);
-    	} else if ($result == -3) {
-	      	loggit(1, "Outline: [".$outline['title']." | ".$outline['url']."] is current.");
-	      	echo "    Outline is current.\n";
-	        unmark_all_outline_feeds_to_purge($outline['id']);
-    	} else {
-	      	loggit(1, "Outline: [".$outline['title']." | ".$outline['url']."] updated.");
-	      	echo "    Outline updated.\n";
 
-		//Extract the feeds from the outline based on type
-		$count = 0;
-		if($outline['type'] == "sopml") {
-			$feeds = get_pub_feeds_from_outline(get_outline_content($outline['id']));
-		} else {
-			$feeds = get_feeds_from_outline(get_outline_content($outline['id']));
-		}
-		echo "Found ".count($feeds)." feeds in this ".$outline['type']." outline.\n";
-		//echo print_r($feeds, true)."\n";
+    // ---------- Social Outlines ------------
+    switch( $outline['type'] ) {
+    case 'sopml':
+    	//Extract the feeds from the outline based on type
+	$count = 0;
+	$feeds = get_pub_feeds_from_outline(get_outline_content($outline['id']));
 
-		//Loop through each feed and add it. Then link all subscribed users to it
-		$newcount = 0;
-		foreach($feeds as $feed) {
-	        	$users = get_outline_subscribers($outline['id']);
-			foreach($users as $user) {
-				loggit(1, "Found feed: [$feed] in outline: [".$outline['title']."].");
-				$feedid = feed_exists($feed);
-				if($feedid == FALSE) {
-					//Feed did not exist
-					$ouchange = TRUE;
-					$newcount++;
-					$fid = add_feed($feed, $user, FALSE, $outline['id']);
-					loggit(1, "Linking feed: [".$fid."] to outline: [".$outline['title']."] for user: [$user].");
-					echo "       Linking feed: [".$fid."] to outline: [".$outline['title']."] for user: [$user].\n";
+	echo "Found ".count($feeds)." pub feeds in this social outline.\n";
+
+	//Loop through each feed and add it. Then link all subscribed users to it
+	$newcount = 0;
+	foreach($feeds as $feed) {
+        	$users = get_outline_subscribers($outline['id']);
+		foreach($users as $user) {
+			loggit(1, "Found feed: [$feed] in outline: [".$outline['title']."].");
+			$feedid = feed_exists($feed);
+			if($feedid == FALSE) {
+				//Feed did not exist
+				$ouchange = TRUE;
+				$newcount++;
+				$fid = add_feed($feed, $user, FALSE, $outline['id']);
+				loggit(1, "Linking feed: [".$fid."] to outline: [".$outline['title']."] for user: [$user].");
+				echo "       Linking feed: [".$fid."] to outline: [".$outline['title']."] for user: [$user].\n";
+			} else {
+				//Links to SOPML outlines should always take priority
+				$oul = get_outline_info(get_feed_outline_by_user($feedid, $user));
+				if( empty($oul) || $oul['type'] != "sopml" ) {
+					loggit(1, "Linking feed: [".$feedid."] to outline: [".$outline['title']."] for user: [$user].");
+					echo "       Linking feed: [".$feedid."] to outline: [".$outline['title']."] for user: [$user].\n";
+					link_feed_to_outline($feedid, $outline['id'], $user);
 				} else {
-					//Links to SOPML outlines should always take priority
-					$oul = get_outline_info(get_feed_outline_by_user($feedid, $user));
-					if( empty($oul) ) {
-						loggit(1, "Linking feed: [".$feedid."] to outline: [".$outline['title']."] for user: [$user].");
-						echo "       Linking feed: [".$feedid."] to outline: [".$outline['title']."] for user: [$user].\n";
-						link_feed_to_outline($feedid, $outline['id'], $user);
-					} else
-					if( $oul['type'] != "sopml" && $outline['type'] == "sopml" ) {
-						loggit(3, "Re-Linking feed: [".$feedid."] to SOPML outline: [".$outline['title']."] for user: [$user].");
-						echo "       Re-Linking feed: [".$feedid."] to SOPML outline: [".$outline['title']."] for user: [$user].\n";
-						link_feed_to_outline($feedid, $outline['id'], $user);
-					}
 					loggit(1, "Feed: [".$feedid."] links to outline: [".$outline['title']."] for user: [$user]. Type: [".$oul['type']." | ".$outline['type']."].");
 					echo "       Feed: [".$feedid."] links to outline: [".$outline['title']."] for user: [$user].\n";
 				}
-				unmark_outline_feed_to_purge($outline['id'], $feedid);
-
 			}
-		}
-	    	echo "    Outline: [".$outline['title']."] had $newcount new feeds.\n";
-
-                //Check for a server declaration
-		if($outline['type'] == "sopml") {
-			$server = get_server_address_from_outline(get_outline_content($outline['id']));
-			if( !empty($server) ) {
-                                $server = trim($server, '/');
-				$serverinfo = fetchUrl('http://'.$server.$getserverinfocgi.'?addr='.$system_fqdn.'&guid='.$cg_main_serverguid);
-				if( $serverinfo != FALSE ) {
-					$serverinfo = json_decode($serverinfo, TRUE);
-					loggit(3, print_r($serverinfo, TRUE));
-					$serverguid = $serverinfo['guid'];
-					if( !empty($serverguid) ) {
-						update_server_address($serverinfo['guid'], $server);
-					}
-				}
-			}
+			unmark_outline_feed_to_purge($outline['id'], $feedid);
 		}
 	}
+    	echo "    Outline: [".$outline['title']."] had $newcount new feeds.\n";
 
-	//If outline was a local list
-	if($outline['type'] == 'list') {
-	        unmark_all_outline_feeds_to_purge($outline['id']);
+        //Check for a server declaration
+	$server = get_server_address_from_outline(get_outline_content($outline['id']));
+	if( !empty($server) ) {
+	        $server = trim($server, '/');
+		if( !isset($servcheck[$server]) ) {
+			$serverinfo = fetchUrl('http://'.$server.$getserverinfocgi.'?addr='.$system_fqdn.'&guid='.$cg_main_serverguid);
+			if( $serverinfo != FALSE ) {
+				$serverinfo = json_decode($serverinfo, TRUE);
+				loggit(3, print_r($serverinfo, TRUE));
+				$serverguid = $serverinfo['guid'];
+				if( !empty($serverguid) ) {
+					update_server_address($serverinfo['guid'], $server);
+				}
+			}
+			$servcheck[$server] = 1;
+		} else {
+			loggit(3, "Already polled server: [$server].");
+		}
 	}
 
 	//Purge feeds that are no longer in the outline
 	$delcount = purge_outline_feeds($outline['id']);
     	echo "    $delcount old feeds purged from outline: [".$outline['title']."].\n";
     	echo "\n";
+        break;
 
-    } else {
-	loggit(1, "No subscribers for: [".$outline['title']."]. Skipping.");
-	echo "    No subscribers. Skipping outline.\n";
+
+    // ---------- Plain OPML files ------------
+    case 'opml':
+	//For plain opml documents, we need to find the diff between old content and new
+	//and create a new pseudo-document off of the difference
+	loggit(1, "The outline type is blank. Skipping.");
+	echo "The outline type is blank. Skipping.\n";
+	break;
+
+
+    // ---------- OPML reading lists ----------
+    case 'list':
+    	//Extract the feeds from the outline
+	$count = 0;
+	$feeds = get_feeds_from_outline(get_outline_content($outline['id']));
+	echo "Found ".count($feeds)." feeds in this reading list.\n";
+
+	//Loop through each feed and add it. Then link all subscribed users to it
+	$newcount = 0;
+	foreach($feeds as $feed) {
+	        $users = get_outline_subscribers($outline['id']);
+		foreach($users as $user) {
+			loggit(1, "Found feed: [$feed] in outline: [".$outline['title']."].");
+			$feedid = feed_exists($feed);
+			if($feedid == FALSE) {
+				//Feed did not exist
+				$ouchange = TRUE;
+				$newcount++;
+				$fid = add_feed($feed, $user, FALSE, $outline['id']);
+				loggit(1, "Linking feed: [".$fid."] to outline: [".$outline['title']."] for user: [$user].");
+				echo "       Linking feed: [".$fid."] to outline: [".$outline['title']."] for user: [$user].\n";
+			} else {
+				//If this feed also exists in an SOPML outline then don't link it again
+				//We want SOPML outlines to always take priority in linking
+				$oul = get_outline_info(get_feed_outline_by_user($feedid, $user));
+				if( empty($oul) && $oul['ownerid'] != $user ) {
+					loggit(1, "Linking feed: [".$feedid."] to outline: [".$outline['title']."] for user: [$user].");
+					echo "       Linking feed: [".$feedid."] to outline: [".$outline['title']."] for user: [$user].\n";
+					link_feed_to_outline($feedid, $outline['id'], $user);
+				} else {
+					loggit(1, "Feed: [".$feedid."] links to outline: [".$outline['title']."] for user: [$user]. Type: [".$oul['type']." | ".$outline['type']."].");
+					echo "       Feed: [".$feedid."] links to outline: [".$outline['title']."] for user: [$user].\n";
+				}
+			}
+			unmark_outline_feed_to_purge($outline['id'], $feedid);
+		}
+	}
+	echo "    Outline: [".$outline['title']."] had $newcount new feeds.\n";
+	unmark_all_outline_feeds_to_purge($outline['id']);
+	break;
+
+
+    // ---------- Unknown Outline Type ----------
+    default:
+	loggit(1, "The outline type is blank. Skipping.");
+	echo "The outline type is blank. Skipping.\n";
+	echo "\n";
+    	break;
     }
-
+    // -- End Switch --
   }
 
 
@@ -134,6 +188,7 @@
   foreach( $users as $u ) {
     $user = $u['id'];
     $feeds = get_pub_feeds($user);
+    if( empty($feeds) ) {  continue;  }
     foreach( $feeds as $feed ) {
       $content = fetchUrl(get_final_url($feed['url']));
       if( $content != FALSE ) {

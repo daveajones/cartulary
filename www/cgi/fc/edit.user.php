bbddac4ae8803588db9bea2b70a522f0ff767833
Dave Jones <sect62@gmail.com>
2013-04-11 16:22:40 +0000

    templatize edit.user cgi init

--- www/cgi/fc/edit.user.php
+++ www/cgi/fc/edit.user.php
@@ -1,50 +1,13 @@
+<?include get_cfg_var("cartulary_conf").'/includes/env.php';?>
+<?include "$confroot/$templates/php_cgi_init.php"?>
+<?include "$confroot/$templates/php_cgi_admin.php"?>
 <?
-//[!------------SECURITY-------------------------------!]
-
-// Includes
-include get_cfg_var("cartulary_conf").'/includes/env.php';
-include "$confroot/$includes/util.php";
-include "$confroot/$includes/auth.php";
-
 // Json header
 header("Cache-control: no-cache, must-revalidate");
 header("Content-Type: application/json");
 $prefs = array();
 $jsondata = array();
 $jsondata['prefname'] = "";
-
-//Get the user id from the session id
-// Valid session?
-if(!is_logged_in()) {
-  loggit(2,"User attempted to hit an admin page without being logged in first.");
-  header("Location: $loginpage");
-  exit(0);
-}
-$uid = get_user_id_from_sid(is_logged_in());
-if(empty($uid) || ($uid == FALSE)) {
-  //Log it
-  loggit(2,"Couldn't retrieve a user id for this session: [$sid].");
-  header("Location: $loginerrorpage?code=3");
-  exit(1);
-}
-
-//See if the user has activated their account yet
-if(!is_user_active($uid)) {
-  //Log it
-  loggit(2,"User tried to access a page without activating first: [$uid | $sid].");
-  header("Location: $errorpage?code=9");
-  exit(1);
-}
-
-//User must be an admin to do this action
-if(!is_admin($uid)) {
-  //Log it
-  loggit(2,"A non-admin user tried to access an admin only page: [$uid | $sid].");
-  header("Location: $errorpage?code=9");
-  exit(1);
-}
-
-//--------------------------------------------------------------------------------
 
 //Get the form input
 $userId = $_REQUEST['hidEditUserId'];

67fa2790bb712f604c3cc481a2443c6867d4ce3b
Dave Jones <sect62@gmail.com>
2013-04-12 14:42:21 +0000

    better output formatting on Articles page search

--- scripts/search-Articles-temp.js
+++ scripts/search-Articles-temp.js
@@ -1,6 +1,6 @@
 <ul>
  {{each(r, result) data}}
- <li><a href="${result.url}">${result.title}</a></li>
+ <li>[ <a href="<?echo $showarticlepage?>?aid=${result.id}"><img class="icon-extlink-1" src="/images/blank.gif" /></a> ] <a href="<?echo $showarticlepage?>?aid=${result.id}">${result.title}</a></li>
  {{/each}}
 </ul>
 

8a3cb2d6dde4d8e8b5b93eb64e06f89d4b280023
Dave Jones <sect62@gmail.com>
2013-04-13 15:21:47 +0000

    2 new cgis for river testing and media river

---
+++ www/cgi/out/list.media.json.php
@@ -0,0 +1,36 @@
+<?include get_cfg_var("cartulary_conf").'/includes/env.php';?>
+<?include "$confroot/$templates/php_cgi_init.php"?>
+<?
+// Json header
+header("Cache-control: no-cache, must-revalidate");
+header("Content-Type: application/json");
+$jsondata = array();
+
+//Was a max value specified?
+if( isset($_REQUEST['max']) ) {
+  $max = $_REQUEST['max'];
+} else {
+  $max = $default_max_list;
+}
+
+//Was a range start specified?
+if( isset($_REQUEST['start']) ) {
+  $start = $_REQUEST['start'];
+} else {
+  $start = 0;
+}
+
+//Get all the users in the system
+$items = get_feed_items_with_enclosures($g_uid, NULL, $max, $start);
+
+
+//--------------------------------------------------------------------------------
+//Give feedback that all went well
+$jsondata['data']['items'] = $items;
+$jsondata['status'] = "true";
+$jsondata['description'] = "List of river items containing media.";
+echo json_encode($jsondata);
+
+return(0);
+?>
+
---
+++ www/cgi/out/news.php
@@ -0,0 +1,19 @@
+<?include get_cfg_var("cartulary_conf").'/includes/env.php';?>
+<?include "$confroot/$templates/php_cgi_init.php"?>
+<?
+// Json header
+header("Cache-control: no-cache, must-revalidate");
+header("Content-Type: application/json");
+$jsondata = array();
+
+// Get the river
+$river = get_river($uid);
+
+// Output
+$jsondata['status'] = "true";
+$jsondata['description'] = "News river.";
+$jsondata['river'] = $river;
+echo json_encode($jsondata);
+loggit(1,"Sending news river for user: [$uid]");
+
+?>

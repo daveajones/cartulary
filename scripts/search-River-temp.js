da1d3af3bbb280fb753f333a4d8826ca440eb2be
Dave Jones <sect62@gmail.com>
2013-04-16 16:47:06 +0000

    cart and mb links in river search results

--- scripts/search-River-temp.js
+++ scripts/search-River-temp.js
@@ -1,6 +1,5 @@
 <ul>
  {{each(r, result) data}}
- <li>[ <a class="mblink" href="${result.url}"><img class="icon-retweet" src="/images/blank.gif" /></a> <a class"cartlink" href="${result.url}"><img class="icon-book-small" src="/images/blank.gif" /></a>] <a href="<?echo $showarticlepage?>?aid=${result.id}">${result.title}</a></li>
+ <li>[ <a class="mblink" href="<?echo $microblogpage?>?description=${encodeURIComponent(result.title)}&link=${encodeURIComponent(result.url)}"><img class="icon-retweet" src="/images/blank.gif" /></a> <a class"cartlink" href="<?echo $cartulizecgi?>?title=${encodeURIComponent(result.title)}&url=${encodeURIComponent(result.url)}"><img class="icon-book-small" src="/images/blank.gif" /></a>] <a href="${result.url}">${result.title}</a></li>
  {{/each}}
 </ul>
-

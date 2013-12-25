c8ff13ffe2c4a0cf2201aad0c556eddb42bb7a9f
Dave Jones <sect62@gmail.com>
2013-12-25 11:29:45 +0000

    handle incoming non-fqdn requests

---
+++ templates/php_redirect_handler.php
@@ -0,0 +1,14 @@
+<?
+  // If the HOST header didn't contain our server's canonical url, we do a lookup
+  // in the redirection table and meta-refresh redirect to that location
+
+  // Includes
+if ( isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] != $system_fqdn ) {
+    require_once "$confroot/$includes/util.php";
+    require_once "$confroot/$includes/net.php";
+    $newurl = get_redirection_url_by_host_name($_SERVER['HTTP_HOST']);
+    if( !empty($newurl) ) {
+        header("Location: ".$newurl);
+        return(0);
+    }
+}

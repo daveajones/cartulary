f6a9abc0bb9f8b4588d8ee8384742bc54429055d
Dave Jones <sect62@gmail.com>
2014-04-26 13:13:57 +0000

    allow all file types in upload

--- www/cgi/in/upload.php
+++ www/cgi/in/upload.php
@@ -1,5 +1,5 @@
-<?include get_cfg_var("cartulary_conf").'/includes/env.php';?>
-<?include "$confroot/$templates/php_cgi_init.php"?>
+<? include get_cfg_var("cartulary_conf") . '/includes/env.php'; ?>
+<? include "$confroot/$templates/php_cgi_init.php" ?>
 <?
 // Json header
 header("Cache-control: no-cache, must-revalidate");
@@ -14,42 +14,34 @@ $uploadDir = "$confroot/$cg_folder_spool/";
 $datestamp = $_POST['datestamp'];
 
 //Process the files coming in
-if( !empty($_FILES) ) {
-  $tempFile   = $_FILES['Filedata']['tmp_name'];
-  $fileName   = cleanFilename($_FILES['Filedata']['name']);
-  $uploadDir  = $uploadDir;
-  $targetFile = $uploadDir . $uid."_".$datestamp."_".$fileName;
-  $targetS3File = $datestamp."_".$fileName;
+if ( !empty( $_FILES ) ) {
+    $tempFile = $_FILES['Filedata']['tmp_name'];
+    $fileName = cleanFilename($_FILES['Filedata']['name']);
+    $uploadDir = $uploadDir;
+    $targetFile = $uploadDir . $uid . "_" . $datestamp . "_" . $fileName;
+    $targetS3File = $datestamp . "_" . $fileName;
 
-  // Validate the file type
-  $fileTypes = array('jpg', 'jpeg', 'gif', 'png', 'mp3', 'pdf'); // Allowed file extensions
-  $fileParts = pathinfo($targetFile);
+    // Validate the file type
+    $fileParts = pathinfo($targetFile);
 
-  // Validate the filetype
-  if( in_array($fileParts['extension'], $fileTypes) ) {
     // Save the file
-    loggit(3, "Upload: moving [".$tempFile."] to [".$targetFile."].");
-    move_uploaded_file($tempFile,$targetFile);
+    loggit(3, "Upload: moving [" . $tempFile . "] to [" . $targetFile . "].");
+    move_uploaded_file($tempFile, $targetFile);
 
     $s3info = get_s3_info($g_uid);
-    if( $s3info != FALSE ) {
-      loggit(3, "Uploading enclosure to S3: ".print_r($targetFile, TRUE));
+    if ( $s3info != FALSE ) {
+        loggit(3, "Uploading enclosure to S3: " . print_r($targetFile, TRUE));
 
-      putFileInS3($targetFile, $targetS3File, $s3info['bucket']."/enc", $s3info['key'], $s3info['secret']);
-      $enclosure = array( 'url' => get_s3_url($uid, '/enc/', $targetS3File),
-                          'length' => filesize($targetFile),
-                          'type' => mime_content_type($targetFile)
-      );
-      loggit(3, "Unlinking file: $targetFile");
-      unlink($targetFile);
+        putFileInS3($targetFile, $targetS3File, $s3info['bucket'] . "/enc", $s3info['key'], $s3info['secret']);
+        $enclosure = array( 'url' => get_s3_url($uid, '/enc/', $targetS3File),
+            'length' => filesize($targetFile),
+            'type' => mime_content_type($targetFile)
+        );
+        loggit(3, "Unlinking file: $targetFile");
+        unlink($targetFile);
 
-      //Give feedback that all went well
-      echo json_encode($enclosure);
-      return(0);
+        //Give feedback that all went well
+        echo json_encode($enclosure);
+        return ( 0 );
     }
-  } else {
-    // The file type wasn't allowed
-    echo 'Invalid file type.';
-  }
 }
-?>

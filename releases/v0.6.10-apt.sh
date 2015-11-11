1ceec96021aa10ff7840118b6edc6813a85f415d
Dave Jones <sect62@gmail.com>
2015-11-11 23:03:07 +0000

    v0.6.10 release notes

---
+++ releases/v0.6.10-apt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##: This script needs to be run with sudo
+ROOT_UID="0"
+
+#Check if run as root
+if [ "$UID" -ne "$ROOT_UID" ] ; then
+	echo "You must run this script with sudo."
+	exit 1
+fi
+export CARTROOT=`echo "<?echo rtrim(get_cfg_var('cartulary_conf'), '/');?>" | php`
+
+##: Refresh apt
+apt-get update
+
+##: Install any new modules needed for this release
+apt-get install -y php5-imap
+php5enmod imap
+
+apt-get install -y php5-xsl
+php5enmod xsl
+
+apt-get install -y nodejs npm
+cd $CARTROOT/aggrivate
+npm install
+
+
+##: This file should be EXEcutable!

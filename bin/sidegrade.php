826cdabd4dd16dcd94469e69b1b7a939afa5090f
Dave Jones <sect62@gmail.com>
2016-04-16 22:36:28 +0000

    upgrade script improvements

--- bin/sidegrade.php
+++ bin/sidegrade.php
@@ -12,7 +12,7 @@ if (($pid = cronHelper::lock()) !== FALSE) {
     echo "Looking for [$sgscript]... ";
     if (file_exists($sgscript)) {
         echo "found. Executing... \n";
-        echo "This may take a while, so please be patient..."
+        echo "This may take a while, so please be patient...";
         $exoutput = shell_exec($sgscript . " 2>&1 > /tmp/v" . $cg_sys_version . "-sidegrade.log");
         echo $exoutput . "\n";
         echo "finished.\n";

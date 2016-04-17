3147383548e1f111159c144546fdb7fce36026a7
Dave Jones <sect62@gmail.com>
2016-04-17 16:16:09 +0000

    trying this again

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -26,43 +26,23 @@ export CARTROOT=`echo "<?echo rtrim(get_cfg_var('cartulary_conf'), '/');?>" | ph
 
 ##: Grab the current repo and extract it
 clear
-echo
-echo '############################################################'
-echo '##----------------------------------------------------------'
-echo '##                                                          '
-echo "##  Grabbing the current $BRANCH release.                   "
-echo '##                                                          '
-echo '##----------------------------------------------------------'
-echo '############################################################'
-echo
 ##: Check the hash on this upgrade script so we can detect changes
 export UPDOLDHASH=`md5sum $CARTROOT/bin/upgrade.sh | awk '{ print $1 }'`
-echo "Update script md5 hash:  $UPDOLDHASH"
 
-echo "Downloading new package..."
 if [ -e "$BRANCH.zip" ] ; then
     rm $BRANCH.zip
 fi
 wget -nv https://github.com/daveajones/cartulary/archive/$BRANCH.zip
 
-echo "Extracting..."
 unzip -q $BRANCH.zip
 
 ##: Stop cron
-echo "Stop cron daemon."
 stop cron
 
 ##: Kill running jobs
-echo "Kill any running php jobs."
 killall php
 
-
-
-
-
-
 ##: Back up the existing install
-echo "Backing up the existing install..."
 tar -zcf ~/cartulary-bak-$BAKDATE.tar.gz $CARTROOT
 
 ##: Get into the repo folder
@@ -72,7 +52,6 @@ cd cartulary-$BRANCH
 cp $CARTROOT/www/newuser.opml /tmp
 
 ##: Put new files in place
-echo "Put the new files in place..."
 cp -R aggrivate $CARTROOT/aggrivate
 cp -R aggrivate/* $CARTROOT/aggrivate
 cp -R bin/*.php $CARTROOT/bin
@@ -86,7 +65,6 @@ cp -R releases/* $CARTROOT/releases
 cp -R www/* $CARTROOT/www
 
 ##: Set permissions
-echo "Set permissions..."
 touch $CARTROOT/logs/error.log
 touch $CARTROOT/logs/debug.log
 touch $CARTROOT/logs/access.log
@@ -102,7 +80,6 @@ cp /tmp/newuser.opml $CARTROOT/www
 cd ..
 
 ##: Kill it
-echo "Clean up..."
 rm -rf cartulary-$BRANCH/
 rm $BRANCH.zip
 
@@ -113,43 +90,25 @@ php $CARTROOT/bin/confcheck.php upgrade silent
 php $CARTROOT/bin/dbcheck.php
 
 ##: Restart cron daemon
-echo "Restart cron daemon..."
 start cron
 
 ##: Run any side-scripts that were shipped with this version
 php $CARTROOT/bin/sidegrade.php
 
-echo "Bounce Apache..."
 service apache2 restart
-
 
 ##: ----- Update this script -----
 export UPDNEWHASH=`md5sum $CARTROOT/bin/new-upgrade.sh | awk '{ print $1 }'`
-echo "New update md5:  $UPDNEWHASH"
-
-echo
-echo 'Upgrade is finished.'
 
 ##: Check hash again for the upgrade script
+echo "Update is finished."
 if [ "$UPDOLDHASH" != "$UPDNEWHASH" ] ; then
     echo
     echo
     echo
-    echo '!!!!!! -- A new version of this upgrade script was just installed.  You should run the upgrade again right now. -- !!!!!!'
+    echo " **** A new version of this upgrade script was just installed.  You should run the upgrade again right now. **** "
     echo
     echo
     echo
 fi
-
-
-
-
-
-
-
-
-
-
-
-
 cp $CARTROOT/bin/new-upgrade.sh $CARTROOT/bin/upgrade.sh

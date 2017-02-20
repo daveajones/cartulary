b605a9a385c26c4ac9614445939cf1ff6e56441f
Dave Jones <sect62@gmail.com>
2017-02-20 04:02:41 +0000

    add new radio page

--- www/radio.php
+++ www/radio.php
@@ -12,6 +12,30 @@ $tree_location = "Radio";
     <title><? echo $tree_location ?></title>
     <? include "$confroot/$templates/$template_html_styles" ?>
     <? include "$confroot/$templates/$template_html_scripts" ?>
+    <style>
+        div.progContainer {
+            position: relative:
+        }
+        hr.progBar {
+            width: 100%;
+            display: block;
+            position: relative;
+            border-color:#333;
+            border-width:1px;
+            z-index: 10;
+        }
+        img.progDot {
+            display: block;
+            z-index: 20;
+            position: relative;
+            top:-26px;
+            left:0px;
+        }
+        div#upNext > ul > li {
+            list-style-type: none;
+            margin-top:8px;
+        }
+    </style>
     <script>
         <?include "$confroot/$scripts/subscribe.js"?>
     </script>
@@ -20,10 +44,10 @@ $tree_location = "Radio";
         var playlist;
         var plcurrent;
         var plcount;
-        var paused = true;
         var currentSound = false;
-        var soundId = 'mySoundId';
+        var soundId = 'mySoundId0';
         var initPlay = true;
+        var gAutoplay = false;
         soundManager.setup({
             url: '/swf/',
             flashVersion: 9, // optional: shiny features (default = 8)
@@ -40,9 +64,18 @@ $tree_location = "Radio";
                         playlist = responseData.data.items;
                         plcount = playlist.length;
                         plcurrent = 0;
-                        for(var i=0 ; i <= playlist.length ; i++ ) {
+
+                        console.log(playlist);
+
+                        //Figure out what to play first
+                        for( var i=0 ; i <= playlist.length ; i++ ) {
                             playUrlAtIndex(plcurrent, false);
                             break;
+                        }
+
+                        //Load the up-next list
+                        for( var i=0 ; i < playlist.length ; i++ ) {
+                            $('div#upNext > ul').append("<li data-index='"+i+"'><button class='playUrl' data-index='"+i+"'><i class='fa fa-play'></i></button> <a style='margin-left:8px;' target='_blank' href='"+playlist[i].url+"'>" + playlist[i].url + "</a></li>");
                         }
                     });
 
@@ -51,6 +84,7 @@ $tree_location = "Radio";
                     console.log("Playing index [" +plindex+ "]")
                     var i = plindex;
                     plcurrent = i;
+                    $('button#pausePlaying > i').removeClass('fa-pause').addClass('fa-play');
 
                     currentSound = soundManager.createSound({
                         id: soundId,
@@ -59,19 +93,62 @@ $tree_location = "Radio";
                         autoPlay: autoplay,
                         onload: function(bSuccess) {
                             if(bSuccess) {
-                                $('div#nowPlaying').html(playlist[i].title + '<hr style="margin:4px;border-color:#888;">' + playlist[i].url);
+                                $('div#nowPlaying').html(playlist[i].title + '<div class="progContainer"><hr class="progBar"><img class="progDot" src="/images/progDot.png" /></div>' + playlist[i].url);
+                                $('div#upNext > ul li').css('font-weight', '').css('color', '');
+                                $('div#upNext > ul li[data-index="'+i+'"]').css('font-weight', 'bold').css('color', '#0FF');
                             } else {
                                 $('div#nowPlaying').html('Error! Skipping... ' + playlist[i].title);
 
                                 playUrlAtNextIndex(autoplay);
+                                currentSound.pause();
                             }
                         },
-                        volume: 50
+                        onpause: function() {
+                            $('button#pausePlaying > i').removeClass('fa-pause').addClass('fa-play');
+                        },
+                        onstop: function () {
+                            $('button#pausePlaying > i').removeClass('fa-pause').addClass('fa-play');
+                        },
+                        onplay: function() {
+                            $('button#pausePlaying > i').removeClass('fa-play').addClass('fa-pause');
+                        },
+                        onresume: function() {
+                            $('button#pausePlaying > i').removeClass('fa-play').addClass('fa-pause');
+                        },
+                        onsuspend: function() {
+                            $('button#pausePlaying > i').removeClass('fa-pause').addClass('fa-play');
+                        },
+                        whileloading: function() {
+                            var e, total = 0;
+                            for (e=0, j=this.buffered.length; e<j; e++) {
+                                // draw current range in progress bar UI?
+                                drawProgressUI(this.buffered[e].start, this.buffered[e].end);
+                                // sum of all ranges vs. whole file duration
+                                total += (this.buffered[e].end - this.buffered[e].start);
+                            }
+                        },
+                        whileplaying: function() {
+                            $(".progContainer .progDot").css('left', ((this.position/this.duration) * 100) + '%');
+                            if(currentSound.playState === 1) {
+                                $('button#pausePlaying > i').removeClass('fa-play').addClass('fa-pause');
+                            }
+                        },
+                        onfinish: function() {
+                            playUrlAtNextIndex(autoplay);
+                        },
+                        volume: 70
                     });
                 }
 
+                $('button#skipBack').click(function() {
+                    console.log(currentSound.position);
+                    currentSound.setPosition(currentSound.position - 15000);
+                    return false;
+                });
+
                 $('button#skipUrl').click(function() {
-                    playUrlAtNextIndex();
+                    console.log(currentSound);
+                    playUrlAtNextIndex(gAutoplay);
                     return false;
                 });
 
@@ -80,7 +157,31 @@ $tree_location = "Radio";
                     return false;
                 });
 
+                $('button#skipForward').click(function() {
+                    console.log(currentSound);
+                    currentSound.setPosition(currentSound.position + 30000);
+                    return false;
+                });
+
+                $(document).on('click', 'button.playUrl', function() {
+                    soundManager.destroySound(soundId);
+                    playUrlAtIndex($(this).data('index'), false);
+                    pausePlaying();
+                    return false;
+                });
+
+                function drawProgressUI(start, end) {
+                    if(start === 0) start++;
+                    start = start|0;
+                    end = end|0;
+                    barPercentage = (start/end) * 100;
+                    console.log("DEBUG: ["+start+"] : " + barPercentage + " : ["+end+"] " + currentSound.durationEstimate);
+
+                    //$('.progContainer .progBar').css('width', barPercentage + "%");
+                }
+
                 function playUrlAtNextIndex(autoplay) {
+                    console.log("playUrlAtNextIndex("+autoplay+")");
                     var autoplay = (typeof autoplay === "undefined") ? true : autoplay;
                     plcurrent++;
 
@@ -88,26 +189,30 @@ $tree_location = "Radio";
                         plcurrent = 0;
                     }
 
-                    currentSound.destruct();
+                    //currentSound.unload();
+                    //soundManager.unload(soundId);
+                    soundManager.destroySound(soundId);
+
+                    currentSound = false;
+
                     playUrlAtIndex(plcurrent, autoplay);
                 }
 
                 function pausePlaying() {
+                    console.log("DEBUG: (currentSound.paused)" + currentSound.paused);
                     if(initPlay) {
                         currentSound.play();
-                        paused = false;
+                        gAutoplay = true;
                         $('button#pausePlaying > i').removeClass('fa-play').addClass('fa-pause');
                         initPlay = false;
                         return false;
                     }
-                    if(paused) {
-                        currentSound.resume(soundId);
-                        paused = false;
-                        $('button#pausePlaying > i').removeClass('fa-play').addClass('fa-pause');
+                    if(currentSound.paused || currentSound.playState === 0) {
+                        if(!initPlay) gAutoplay = true;
+                        currentSound.play();
                     } else {
-                        currentSound.pause(soundId);
-                        paused = true;
-                        $('button#pausePlaying > i').removeClass('fa-pause').addClass('fa-play');
+                        if(!initPlay) gAutoplay = false;
+                        currentSound.pause();
                     }
                 }
             }
@@ -129,9 +234,14 @@ $tree_location = "Radio";
 <div id="nowPlaying" style="background: white; min-height: 40px; padding:10px;"></div>
 
 <center style="margin:20px;">
+    <button id="skipBack" style="padding: 8px;"><i class="fa fa-backward fa-2x"></i></button>
     <button id="pausePlaying" style="padding:8px;"><i class="fa fa-play fa-2x"></i></button>
     <button id="skipUrl" style="padding:8px;"><i class="fa fa-fast-forward fa-2x"></i></button>
+    <button id="skipForward" style="padding: 8px;"><i class="fa fa-forward fa-2x"></i></button>
 </center>
+
+<label for="upNext">Up Next:</label>
+<div id="upNext" style="background: white; min-height: 40px; padding:10px;"><ul class="list-unstyled"></ul></div>
 
 <? //--- Include the footer bar html fragments -----------?>
 <? include "$confroot/$templates/$template_html_footerbar" ?>

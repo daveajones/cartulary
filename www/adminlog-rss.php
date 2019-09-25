f9f7089eb721c84ae867ae456188a7c7bbcd1893
Dave Jones <sect62@gmail.com>
2019-09-25 21:37:45 +0000

    admin feed

--- www/adminlog-rss.php
+++ www/adminlog-rss.php
@@ -1,17 +1,19 @@
-<?include get_cfg_var("cartulary_conf") . '/includes/env.php'; ?>
-<?include "$confroot/$templates/php_page_noauth.php" ?>
+<? include get_cfg_var("cartulary_conf") . '/includes/env.php'; ?>
+<? include "$confroot/$templates/php_page_noauth.php" ?>
 <?
+
 //Check for an admin feed token
+if(!isset($_REQUEST['t']) || empty($_REQUEST['t']) || $_REQUEST['t'] != $admin_feed_check_token) {
+    exit(1);
+}
 
 //Did they specify how many articles they wanted in the list?
-if ( isset( $_REQUEST['max'] ) ) {
+if (isset($_REQUEST['max'])) {
     $max = $_REQUEST['max'];
-} else if ( isset( $prefs['maxlist'] ) ) {
+} else if (isset($prefs['maxlist'])) {
     $max = $prefs['maxlist'];
 } else {
     $max = $default_max_list;
 }
-
-$tree_location = "Admin Log Feed";
 
 echo build_admin_log_rss_feed($max);
